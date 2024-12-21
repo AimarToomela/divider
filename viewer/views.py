a1bad12b44059d618067b762563c9a316859f096
Your Name <toomelaaimar@gmail.com>
2024-12-21 10:45:42 +0200

    Divider exercise

--- viewer/views.py
+++ viewer/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.template.defaultfilters import yesno
+
 
 # Create your views here.
+def divide(request):
+    try:
+        x = float(request.GET.get('x', 0))
+        y = float(request.GET.get('y', 0))
+
+        if y == 0:
+            return JsonResponse({"error": "You can't divide by 0!"}, status=400)
+        result = x / y
+        return JsonResponse({"result": result})
+
+    except ValueError:
+        return JsonResponse({"error": "Invalid input. Please provide numbers."}, status=400)
